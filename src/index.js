c8929f473d8287cbde64facac3ab0aee09463101
Naufal Chandira <hadinaufal11@gmail.com>
2024-03-28 01:23:06 +0700

    error dinavbar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/style.css";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Import Bootstrap JavaScript
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
