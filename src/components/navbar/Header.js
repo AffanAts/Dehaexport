e5dff8fdddc52217117bf8b4a18242449eb8a464
Naufal Chandira <hadinaufal11@gmail.com>
2024-03-28 02:09:23 +0700

    add icon

--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-expand-lg bg-color">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg bg-color container-fluid">
+      <div className="container">
         <button
-          className="navbar-toggler"
+          className="navbar-toggler ms-auto"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNavAltMarkup"
@@ -16,25 +17,36 @@ export default function Header() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div
-          className="collapse navbar-collapse my-auto"
+          className="collapse navbar-collapse justify-content-center"
           id="navbarNavAltMarkup"
         >
           <div className="navbar-nav">
-            <a className="nav-link active" aria-current="page" href="#">
-              Home
+            <a
+              className="nav-link active mx-auto px-3"
+              aria-current="page"
+              href="#"
+            >
+              About
             </a>
-            <a className="nav-link" href="#">
-              Features
+            <a className="nav-link mx-auto px-3" href="#">
+              Gallery
             </a>
-            <a className="navbar-brand" href="#">
-              Navbar
+            <a className="nav-link mx-auto px-3" href="#">
+              Products
             </a>
-            <a className="nav-link" href="#">
-              Pricing
+            <a className="navbar-brand mx-auto px-4" href="#">
+              Web Export
             </a>
-            <a className="nav-link disabled" aria-disabled="true">
-              Disabled
+            <a className="nav-link mx-auto px-3" href="#">
+              Team
             </a>
+            <a className="nav-link disabled mx-auto" aria-disabled="true">
+              Blog
+            </a>
+            <a className="nav-link mx-auto px-3" href="#">
+              Team
+            </a>
+            <FontAwesomeIcon icon={faInstagram} />
           </div>
         </div>
       </div>
