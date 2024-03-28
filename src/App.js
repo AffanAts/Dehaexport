a34a23287959800c2863ad3d212508fac3501436
Affan Haidar <affanhaidar232@gmail.com>
2024-03-29 01:51:49 +0700

    add some component file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react"; // Mengimport React
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./page/Main";
-import About from "./page/About";
 import Header from "./components/navbar/Header";
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} /> {/* Menghapus 'exact' */}
-          <Route path="/about" element={<About />} />{" "}
+          {/* <Route path="/about" element={<About />} />{" "} */}
           {/* Mengubah path menjadi '/about' */}
         </Routes>
       </BrowserRouter>
---
+++ src/components/profilpage/LandingPage.js
@@ -0,0 +1,44 @@
+import homeBackground from "../../assets/img/home.jpg";
+
+export default function LandingPage() {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${homeBackground})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        width: "100vw", // Set width to viewport width
+        height: "100vh", // Set height to viewport height
+        padding: 0,
+        margin: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: 50,
+          color: "white", // Text color
+          textAlign: "center",
+        }}
+      >
+        The World Class Spices from Indonesia
+      </h1>
+      <p
+        style={{
+          color: "white", // Text color
+          textAlign: "center",
+          maxWidth: 600, // Limiting width of text
+          padding: "0 20px", // Adding padding for better readability
+        }}
+      >
+        Spices are Indonesia’s fourth largest export commodity, after shrimp,
+        fish, and coffee. As one of the world’s spice producers, Indonesia has a
+        great opportunity as a supplier of the world’s spices to contribute to
+        the Indonesian economy.
+      </p>
+    </div>
+  );
+}
